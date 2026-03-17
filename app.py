4c13dee02b7a6adf63c0e1529bb73a6deabbd066
alexrosumieres-sudo <alexrosumieres@gmail.com>
2026-03-17 12:02:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -878,7 +878,8 @@ else:
                 # Configuración de la IA
                 api_key_ia = st.secrets["GEMINI_API_KEY"]
                 genai.configure(api_key=api_key_ia)
-                model_ia = genai.GenerativeModel('gemini-1.5-flash')
+                # Prueba con este nombre que es el estándar más compatible
+                model_ia = genai.GenerativeModel('models/gemini-1.5-flash')
     
                 # Inicializar historial de chat si no existe
                 if "messages_ia" not in st.session_state:
